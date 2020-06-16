1b3b24558a54397bf89068387a2261b49efa16d6
frankjrp <frankjrp@gmail.com>
2020-06-16 02:59:32 -0300

    Update get-started.js
    Correção da variável 'sum'.

--- get-started.js
+++ get-started.js
@@ -15,7 +15,7 @@ function onClick() {
   function updateLabel() {
     var addend1 = getNumber1();
     var addend2 = getNumber2();
-    var sum = addend1 + addend2;
+    var sum = parseInt(addend1) + parseInt(addend2);
     label.textContent = addend1 + ' + ' + addend2 + ' = ' + sum;
   }
   function getNumber1() {
