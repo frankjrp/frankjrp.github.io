5cccb88b50a3946587dd424ec8197cab0ecc438f
frankjrp <frankjrp@gmail.com>
2020-07-25 04:33:22 -0300

    Menu hamburguer no mobile

---
+++ scriptMobile.js
@@ -0,0 +1,17 @@
+const menuMobile = document.getElementById("checkbox-menu");
+const links = document.querySelectorAll(".mobile_links a")
+
+document.querySelector(".menu_mobile label").addEventListener("click", () => {
+
+    if (menuMobile.checked) {
+        document.querySelector(".mobile").classList.remove("active");
+    } else {
+        document.querySelector(".mobile").classList.add("active");
+    }
+})
+
+for (let link of links) {
+    link.addEventListener("click", () => {
+        menuMobile.checked = false;
+    })
+}
