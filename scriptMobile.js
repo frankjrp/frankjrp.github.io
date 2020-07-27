c0e8d7446e8646ad58532e5dbe175dd9b87291c3
frankjrp <frankjrp@gmail.com>
2020-07-27 03:26:59 -0300

    Ajustes na exibição

--- scriptMobile.js
+++ scriptMobile.js
@@ -1,12 +1,14 @@
-const menuMobile = document.getElementById("checkbox-menu");
+const menuMobile = document.getElementById("checkbox-menu")
 const links = document.querySelectorAll(".mobile_links a")
 
 document.querySelector(".menu_mobile label").addEventListener("click", () => {
 
     if (menuMobile.checked) {
         document.querySelector(".mobile").classList.remove("active");
+        document.querySelector(".description").classList.remove("active");
     } else {
         document.querySelector(".mobile").classList.add("active");
+        document.querySelector(".description").classList.add("active");
     }
 })
 
