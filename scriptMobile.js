97617aa15ac6088e3beeed4c64ea559675bcb514
frankjrp <frankjrp@gmail.com>
2022-08-21 00:16:53 -0300

    Menu novo layout

--- scriptMobile.js
+++ scriptMobile.js
@@ -1,11 +1,16 @@
 const menuMobile = document.getElementById("checkbox-menu")
 const links = document.querySelectorAll("header nav ul li a")
+const menu = document.querySelector("header nav .menu")
 
 document.querySelector(".menu_mobile label").addEventListener("click", () => {
 
     if (menuMobile.checked) {
+        document.querySelector("footer .footer-container").classList.remove("hide")
+        menu ? menu.classList.remove("active") : null
         document.querySelector("header nav .navbar").classList.remove("active");
     } else {
+        document.querySelector("footer .footer-container").classList.add("hide")
+        menu ? menu.classList.add("active") : null
         document.querySelector("header nav .navbar").classList.add("active");
     }
 })
