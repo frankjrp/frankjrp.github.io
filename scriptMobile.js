577d5469cae5e4a77bf18685eb76b00bf34c5e63
frankjrp <frankjrp@gmail.com>
2024-03-18 02:40:56 -0300

    Ajustes no overflow com o menu aberto

--- scriptMobile.js
+++ scriptMobile.js
@@ -1,16 +1,33 @@
 const menuMobile = document.getElementById("checkbox-menu")
 const links = document.querySelectorAll("header nav ul li a")
 const menu = document.querySelector("header nav .menu")
+const container = document.querySelector(".container")
+const body = document.querySelector("body")
 const navbar = document.querySelector("header nav .navbar")
 const footer = document.querySelector("footer .footer-container")
 
 document.querySelector(".menu_mobile label").addEventListener("click", () => {
 
     if (menuMobile.checked) {
+        const savedScrollPosition = localStorage.getItem('scrollPosition')
+
+        body.classList.remove("hidden")
+        container.classList.remove("fixed")
         footer.classList.remove("hide")
         menu ? menu.classList.remove("active") : null
         navbar.classList.remove("active")
+
+        window.scrollTo({
+            top: savedScrollPosition,
+            behavior: "auto"
+        })
+
     } else {
+        const scrollPos = window.scrollY
+        localStorage.setItem('scrollPosition', scrollPos)
+
+        body.classList.add("hidden")
+        container.classList.add("fixed")
         footer.classList.add("hide")
         menu ? menu.classList.add("active") : null
         navbar.classList.add("active")
