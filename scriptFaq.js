999a2c1e2c4e3cda680f8a70d61277f3da6b703f
frankjrp <frankjrp@gmail.com>
2024-04-18 03:12:52 -0300

    Ajustes na abertura de uma pergunta

--- scriptFaq.js
+++ scriptFaq.js
@@ -1,14 +1,29 @@
 const questions = document.querySelectorAll(".question")
+const btns = document.querySelectorAll(".question h2")
 
-questions.forEach((question) => {
-    question.querySelector("h2").addEventListener("click", () => {
-        question.classList.toggle("open")
+function handleQuestions(parent) {
+    const content = parent.querySelector(".height")
 
-        let content = question.querySelector(".height")
+    if (parent.classList.contains("open")) {
+        parent.classList.remove("open")
+        content.style.maxHeight = null
+    } else {
+        questions.forEach(question => {
+            if (question.classList.contains("open")) {
+                question.classList.remove("open")
+                question.querySelector(".height").style.maxHeight = null
+            }
+        })
 
-        question.classList.contains("open") ?
-            content.style.maxHeight = content.scrollHeight + "px"
-            :
-            content.style.maxHeight = null
+        parent.classList.add("open")
+        content.style.maxHeight = content.scrollHeight + "px"
+    }
+}
+
+btns.forEach(btn => {
+    btn.addEventListener("click", (e) => {
+        const parent = e.target.parentNode
+
+        handleQuestions(parent)
     })
 })
